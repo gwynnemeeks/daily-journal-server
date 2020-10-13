c12228213598724ff71760b63acf9dc895fb2bdd
Gwynne Meeks <meeks.gwynne@gmail.com>
2020-10-13 10:27:38 -0500

    sql database tables and seed data setup

---
+++ dailyjournal.sql
@@ -0,0 +1,13 @@
+CREATE TABLE `Mood` (
+    `id`    INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    `label`    TEXT NOT NULL
+);
+
+CREATE TABLE `Entries` (
+	`id`	INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+	`concept`	TEXT NOT NULL,
+	`entry`	TEXT NOT NULL,
+    `date` TEXT NOT NULL,
+    `mood_id` INTEGER NOT NULL,
+    FOREIGN KEY (`mood_id`) REFERENCES `Mood`(`id`)
+);
